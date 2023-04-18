7b107f8bca09f0306e9269aaa3a3a6d3ed64ab63
jakesungjaekim <codejakecarry@gmail.com>
2023-04-18 22:41:37 +0900

    Modfiy: import 수정

--- src/pages/Home/components/SearchBar/SearchBar.jsx
+++ src/pages/Home/components/SearchBar/SearchBar.jsx
@@ -1,6 +1,4 @@
 import React from 'react'
-// import DropdownMenu from '../../../../components/HeadlessUi/DropdownMenu'
-// import ListBox from '../../../../components/HeadlessUi/ListBox'
 
 const SearchBar = () => {
   return (
